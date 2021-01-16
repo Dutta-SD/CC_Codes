d6a38a9e30d4d156da4c643a1c9496470e700fd0
Dutta-SD <duttasandip11100@gmail.com>
2021-01-16 21:12:22 +0530

    01/16/21 - New Problems Added

--- Practice/CF_Practice/Even_Odd_Game.cpp
+++ Practice/CF_Practice/Even_Odd_Game.cpp
@@ -4,17 +4,17 @@ using namespace std;
 void solve(){
     long l;
     cin >> l;
-    vector<long> scores(l);
+    vector <long> scores(l);
     for(auto &i : scores){
         cin >> i;
     }
     sort(scores.rbegin(), scores.rend());
-    long alice = 0, bob = 0;
+    long long alice = 0, bob = 0;
     for(long i = 0 ;i < l; ++i){
-        if(i & 1){
-            bob += (scores[i] & 1 ? scores[i] : 0);
+        if(i % 2 == 1){
+            bob += (scores[i] % 2 == 1 ? scores[i] : 0);
         } else {
-            alice += (scores[i] & 1 ? 0 : scores[i]);
+            alice += (scores[i] % 2 == 1 ? 0 : scores[i]);
         }
     }
 
