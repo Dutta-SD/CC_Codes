037a43fbbbb67ac2ba7a0f22c811ae7c5101b2d5
Dutta-SD <duttasandip11100@gmail.com>
2021-03-07 00:49:14 +0530

    03/07/21 - New Problems Added

--- Competitions/Atcoder_ABC_194/C.py
+++ Competitions/Atcoder_ABC_194/C.py
@@ -1,7 +1,9 @@
-from itertools import combinations
-
 n = int(input())
 
-a = map(int, input().split())
+a = list(map(int, input().split()))
 
-print(sum([(i - j)*(i-j) for i, j in combinations(a, 2)]))
+x = sum([i*i for i in a])
+
+s = sum(a)
+
+print((n*x) - (s*s))
