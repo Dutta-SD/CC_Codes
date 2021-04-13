f43b469356a76c799c05ac86092d564b080f8e8e
Dutta-SD <duttasandip11100@gmail.com>
2021-04-13 08:43:41 +0530

    04/13/21 - New Problems Added

--- Practice/CSES/GridPath.cpp
+++ Practice/CSES/GridPath.cpp
@@ -14,11 +14,15 @@ int main(){
     cin >> n;  
     vector < vector<char> > grid (n, vector <char> (n, 0));
 
-    for(auto &i: grid) {
-        for(auto &j : i){
-            cin >> j;
+    for(int i = 0; i < n; ++i){
+        string s;
+        cin >> s;
+        for(int j = 0; j < n; ++j){
+            grid[i][j] = s[j];
         }
     }
+
+    /* INPUT ENDS*/   
 
     // Bottom up dp approach
 
@@ -26,15 +30,24 @@ int main(){
 
     numWays[0][0] = 1;
 
+    if(grid[0][0] == '*'){
+        cout << 0;
+        return 0;
+    }
     for (int i = 0; i < n; ++i){
         for(int j = 0; j < n; ++j){
             if(grid[i][j] == '.'){
-                if(grid[i-1][j] == '.') grid[i][j] += 
-            }
-
+                if(i > 0){
+                    // has top way
+                    (numWays[i][j] += numWays[i-1][j]) %= MOD;
+                }
+                if(j > 0){
+                    (numWays[i][j] += numWays[i][j-1]) %= MOD;
+                }
+            }            
         }
     }
 
-    cout << numCombinations[n][value];
+    cout << numWays[n-1][n-1];
     return 0;    
 }
