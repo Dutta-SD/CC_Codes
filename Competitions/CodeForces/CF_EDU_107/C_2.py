73826a1749c6c0b0f1ed74576550d8a3149389c3
Dutta-SD <duttasandip11100@gmail.com>
2021-04-14 15:45:52 +0530

    04/14/21 - New Problems Added

--- Competitions/CodeForces/CF_EDU_107/C_2.py
+++ Competitions/CodeForces/CF_EDU_107/C_2.py
@@ -1,14 +1,9 @@
 input()
 colors = input().split()
 queries = input().split()
-# INPUT END
 
 for q in queries:
-    # print(f"COLOR INITIAL : {colors}")
     idx = colors.index(q)
     print(idx + 1, end = " ")
-    a = [colors[idx]]
-    b = colors [:idx]
-    c = colors[idx + 1 : ]
-    colors = a + b + c
-    # print(f"COLOR FINAL : {colors}")
+    for e in range(idx , 0 , -1):
+        colors[e], colors[e-1] = colors[e-1], colors[e]
