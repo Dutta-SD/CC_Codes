4fc92927d8d9c118807c03b0aba524d6a0adbfa7
Dutta-SD <duttasandip11100@gmail.com>
2021-04-15 16:07:41 +0530

    04/15/21 - New Problems Added

---
+++ Practice/CSES/Concert_Tickets.cpp
@@ -0,0 +1,31 @@
+# include <bits/stdc++.h>
+# pragma GCC optimize("Ofast")
+# pragma GCC target("avx,avx2,fma")
+using namespace std;
+
+int main (){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    long n, m, temp;
+    cin >> n >> m;
+    multiset<long> v;
+
+    for(long i = 0; i < n; ++i){
+        cin >> temp;
+        v.insert(temp);
+    }
+
+    while(m--){
+        cin >> temp;
+        auto i = v.upper_bound(temp);
+        if(i == v.end()){
+            cout << -1 << "\n";
+        } else {
+            cout << *i <<"\n";
+            v.erase(i);
+        }
+    }
+    return 0;
+}
