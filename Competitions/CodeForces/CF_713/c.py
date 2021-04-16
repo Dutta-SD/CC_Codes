d6d0f7b44fc9d221abebabde00778af6f8fbb8e4
Dutta-SD <duttasandip11100@gmail.com>
2021-04-16 16:15:34 +0530

    04/16/21 - New Problems Added

--- Competitions/CodeForces/CF_713/c.py
+++ Competitions/CodeForces/CF_713/c.py
@@ -1,97 +1,108 @@
 from collections import Counter
 
-# debug = False
-
 for t in range(int(input())):
 
-    # print("-"*80) if debug else None
     #---------------------INPUT------------------
     a, b = map(int, input().split())
     s = list(input())
     counts = Counter(s)
     #---------------------------------------------
 
-    # print(f"---> Count initial {counts}") if debug else None
-    # print(f"a = {a}, b = {b}") if debug else None
+    # Additional check  
+    flag = True
+
+    # Initial counts
+    if counts['0']  > a or counts['1'] > b or a + b != len(s):
+        print(-1)
+        continue
+
+    if '?' not in s and s == s[::-1]:
+        print(''.join(s))
+        continue
 
     i = 0
     j = len(s) - 1
 
-    while i <= j:
-        if s[i] == s[j] and s[i] != '?':
-            i += 1
-            j -= 1
-            continue
-        
-        if (s[i] == '?' and s[j] == '1'):
-            s[i] = '1'
-            counts['1'] += 1
+    # iterate, if center then handle separately
+    while i < j:
+        # 0 ? case, if unequal bad. Else nothing
+        if s[i] in ['0', '1'] and s[j] in ['0', '1'] and s[i] != s[j]:
+            # If unequal then action
+            flag = False
 
-        elif (s[j] == '?' and s[i] == '1'):
-            s[j] = '1'
-            counts['1'] += 1
+        # Single ? case
+        if (s[i] == '?' and s[j] != '?') or (s[j] == '?' and s[i] != '?'):
+            if (s[i] == '?' and s[j] == '1'):
+                s[i] = '1'
+                counts['1'] += 1
+                counts['?'] -= 1
+    
+            elif (s[j] == '?' and s[i] == '1'):
+                s[j] = '1'
+                counts['1'] += 1
+                counts['?'] -= 1
 
-        elif (s[j] == '?' and s[i] == '0'):
-            s[j] = '0'
-            counts['0'] += 1
+    
+            elif (s[j] == '?' and s[i] == '0'):
+                s[j] = '0'
+                counts['0'] += 1
+                counts['?'] -= 1
 
-        elif (s[i] == '?' and s[j] == '0'):
-            s[i] = '0'
-            counts['0'] += 1
+    
+            elif (s[i] == '?' and s[j] == '0'):
+                s[i] = '0'
+                counts['0'] += 1
+                counts['?'] -= 1
 
-        # double question mark
-        else:
-            if i == j:
-                if a % 2 == 1 and b % 2 == 1:
-                    counts['0'] = -1
-                elif a % 2 == 1 and counts['0']+1 <= a:
-                    s[i] = '0'
-                    counts['0'] += 1
-
-                elif b % 2 == 1 and counts['1']+1 <= b:
-                    s[i] = '1'
-                    counts['1'] += 1
-
-                else:
-                    counts['0'] = -1
-
+        # Double Question mark case
+        # Never do we get same position, so i != j
+        if s[i] == '?' and s[j] == '?':
+            if a - counts['0'] >= 2:
+                s[i] = '0'
+                s[j] = '0'
+                counts['0'] += 2
+                counts['?'] -= 2
+            
+            elif b - counts['1'] >= 2:
+                s[i] = '1'
+                s[j] = '1'
+                counts['1'] += 2
+                counts['?'] -= 2
+            
             else:
-                n = None
-                if counts['0'] + 2 <= a:
-                    n = '0'
-                    s[i] = n
-                    s[j] = n
-                    # print(f"Position {i} : Character {s[i]}") if debug else None
-                    # print(f"Position {j} : Character {s[j]}") if debug else None
-
-                    counts[n] += 2
-
-                elif counts['1'] + 2 <= b:
-                    n = '1'
-                    s[i] = n
-                    s[j] = n
-                    # print(f"Position {i} : Character {s[i]}") if debug else None
-                    # print(f"Position {j} : Character {s[j]}") if debug else None
-
-                    counts[n] += 2
-                
-                else:
-                    counts['0'] = -1
+                flag = False
+        
+        # Stepping of loop
         i += 1
         j -= 1
 
-    # print(f"Counter is Now : {counts}") if debug else None
+    # Center handling, if odd
+    if len(s) % 2 == 1:
+        position = int(len(s) / 2)
+        if s[position] == '?':
+            if a - counts['0'] == 1:
+                s[position] = '0'
+                counts['0'] += 1
+                counts['?'] -= 1
+
+            elif b - counts['1'] == 1:
+                s[position] = '1'
+                counts['1'] += 1
+                counts['?'] -= 1
+
+            else:
+                flag = False
 
 
-    # print(f"FINAL LIST IS : {s}") if debug else None
-    
-    if counts['1'] == b and counts['0'] == a:
-        # print("Final Answer is: ") if debug else None
 
-        print(''.join(s))
+    if False:
+        print(counts)
+
+
+    if flag:
+        if counts['0'] == a and counts['1'] == b:
+            print(''.join(s))
+        else:
+            print(-1)
     else:
-        # print("Final Answer is: ") if debug else None
-
         print(-1)
-
-    # print("-"*80) if debug else None
