6d83f88e541d6bc16bf32ed9a8591131d7c6fde5
Dutta-SD <duttasandip11100@gmail.com>
2021-04-21 20:25:13 +0530

    04/21/21 - New Problems Added

---
+++ Practice/CSES/TO_3.py
@@ -0,0 +1,21 @@
+classes = {
+  0 : 0,
+  1 : 0,
+  2 : 0
+}
+n = input()
+s = 0
+
+
+for i in n:
+  classes[int(i) % 3] += 1
+  s += int(i)
+
+# print(classes)
+
+if s % 3 == 0:
+  print(0)
+elif classes[s % 3] == 0:
+  print(-1)
+else:
+  print(1)
