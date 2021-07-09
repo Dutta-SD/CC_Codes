72fb86aa79c3522d8e1ccf2441e8bcd0b4fe3a61
Dutta-SD <duttasandip11100@gmail.com>
2021-07-09 11:59:38 +0530

    07/09/21 - New Problems Added

--- Practice/Striver_Sheet/Day_1/Merge_Inplace.cpp
+++ Practice/Striver_Sheet/Day_1/Merge_Inplace.cpp
@@ -1,3 +1,41 @@
 # include <bits/stdc++.h>
 using namespace std;
 
+void printArray(int arr[], int n){
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void solve(){
+    int a1[5] = {1, 3, 4, 8, 9};
+    int a2[3] = {2, 6, 7};
+
+    for(int i = 0; i < 5; ++i){
+
+        if(a1[i] > a2[0]){
+            // If more than beginning, then swap
+            // then insert in proper position
+            swap(a1[i], a2[0]);
+            int f = a2[0];
+            int j;
+            for(j = 1; j < 3 && a2[j] < f; ++j){
+                a2[j-1] = a2[j];
+            }
+
+            a2[j-1] = f;
+        }
+    }
+
+    printArray(a1, 5);
+    printArray(a2, 3);
+
+}
+
+int main(){
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}
